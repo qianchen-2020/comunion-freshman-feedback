7ba2b595baaeb9e5217397143d5292dc495b251b
erguotou <erguotou525@gmail.com>
2021-02-04 15:49:11 +0800

    text: 文案修改

--- src/components/Done.tsx
+++ src/components/Done.tsx
@@ -5,8 +5,8 @@ export default defineComponent({
   props: {},
   setup() {
     return () => (
-      <div class="py-24 text-center ">
-        <p class="text-2xl text-blue-500">欢迎你加入Comunion！</p>
+      <div class="py-24">
+        <p class="text-2xl text-blue-500 text-center">欢迎你加入Comunion！</p>
         <p class="mt-4">我们将会附带你填写的消息给你的 Mentor 发送邮件。</p>
         <p class="mt-1">你可以主动联系你的 Mentor 或者等待 Mentor 为你开通相关帐号。</p>
         <p class="mt-4 italic text-sm">最后希望你能在 Comunion 有所收获、有所成就。</p>
